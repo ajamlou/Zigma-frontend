fefabcb3fb396c521509ac2585640f4498515d25
n3reides <dahlstedtolle@gmail.com>
2019-04-12 11:56:43 +0200

    advertcreation

--- lib/pages/advert_creation.dart
+++ lib/pages/advert_creation.dart
@@ -5,9 +5,28 @@ class advertCreation extends StatefulWidget {
 }
 
 class advertCreationState extends State<advertCreation> {
+  final GlobalKey<FormState> _advertKey =  GlobalKey<FormState>();
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Color(0xFFECE9DF),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget> [
+          Form(
+            key: _advertKey,
+            child: Column(
+              children: <Widget> [
+                Text(), //Här ska information om vad som ska ingå i textfältet skrivas
+                TextFormField(
 
+            ),// Här ska textfältet kontrolleras
+          ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
