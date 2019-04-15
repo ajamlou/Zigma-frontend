754dea035ea48f1743243ac2d6a8ec1cb5cad55e
Max <max.netterberg@gmail.com>
2019-04-15 12:21:02 +0200

    fixat med söket

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,62 +1,10 @@
 import 'package:flutter/material.dart';
 import './advert.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-class SearchData extends StatefulWidget {
-  @override
-  SearchDataState createState() => SearchDataState();
-}
-
-class SearchDataState extends State<SearchData> {
-  final String url = "https://jsonplaceholder.typicode.com/posts";
-  List data;
-
-  Future<String> getData() async {
-    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
-    setState(() {
-      data = json.decode(res.body);
-    });
-    return "Success!";
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: ListView.builder(
-        itemCount: getItemCount(data),
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            child: Text(data[index]["id"].toString()),
-          );
-        },
-      ),
-    );
-  }
-
-  int getItemCount(data) {
-    if (data == null) {
-      return 0;
-    }
-    return data.length;
-  }
-
-  List sendData(){
-    return data;
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    this.getData();
-  }
-}
 
 
 class SearchPage extends SearchDelegate<Advert> {
-  //List<Advert> _adverts = adverts;
+  final List data;
+  SearchPage({this.data});
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -86,8 +34,11 @@ class SearchPage extends SearchDelegate<Advert> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container(
-//      child: Text(SearchData().sendData())
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (BuildContext context, int index){
+        return Text(data[index]["id"].toString());
+      },
     );
   }
 }
