76e5c540b7b8ee1b3c68ce78bc426effb6201e55
n3reides <dahlstedtolle@gmail.com>
2019-04-16 16:59:00 +0200

    lol

--- lib/pages/advert_creation.dart
+++ lib/pages/advert_creation.dart
@@ -44,149 +44,164 @@ class advertCreationState extends State<advertCreation>
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Color(0xFFECE9DF),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 25.0),
-                child: IconButton(
-                  color: Color(0xFF96070a),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: Icon(Icons.arrow_back),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 25.0, left: 50.0),
-                child: Text('Lägg till en ny annons'),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                alignment: Alignment(0.0, 0.0),
-                child: _image == null
-                    ? Text('No image Selected')
-                    : Container(
-                        constraints: BoxConstraints(
-                          maxHeight: 150.0,
-                          maxWidth: 150.0,
-                          minWidth: 150.0,
-                          minHeight: 150.0,
-                        ),
-                        child: Image.file(_image),
-                      ),
-              ),
-              FloatingActionButton(
-                onPressed: getImageCamera,
-                tooltip: 'Pick Image',
-                child: Icon(Icons.add_a_photo),
-              ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 30.0, top: 10.0, right: 30.0, bottom: 10.0),
-            child: Form(
-              key: _advertKey,
-              child: new Container(
-                decoration: new BoxDecoration(
-                  shape: BoxShape.rectangle,
+    return Container(
+      color: Color(0xFFFFFFFF),
+      child: new Scaffold(
+        resizeToAvoidBottomPadding: false,
+        backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60.0),
+          child: AppBar(
+            iconTheme: IconThemeData(color: Color(0xff96070a)),
+            elevation: 0.0,
+            backgroundColor: Color(0xff96070a),
+            title: Text('Lägg till en ny annons',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
                   color: Color(0xFFFFFFFF),
-                  gradient: new LinearGradient(
-                    colors: [Color(0xFF96070a), Color(0xFFFFFFFF)],
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.elliptical(20.0, 20.0)),
-                ),
-                child: Column(
+                )),
+            centerTitle: true,
+            leading: new Container(
+              child: IconButton(
+                color: Color(0xFFFFFFFF),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.arrow_back),
+              ),
+            ),
+            actions: <Widget>[],
+          ),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    new TextFormField(
-                      maxLines: 1,
-                      keyboardType: TextInputType.text,
-                      autofocus: false,
-                      decoration: new InputDecoration(
-                        hintText: 'Titel',
-                      ),
-                      validator: (value) =>
-                          value.isEmpty ? 'Title can\'t be empty' : null,
-                      onSaved: (value) => _title = value,
+                    Container(
+                      alignment: Alignment(0.0, 0.0),
+                      child: _image == null
+                          ? Text('No image Selected')
+                          : Container(
+                              constraints: BoxConstraints(
+                                maxHeight: 150.0,
+                                maxWidth: 150.0,
+                                minWidth: 150.0,
+                                minHeight: 150.0,
+                              ),
+                              child: Image.file(_image),
+                            ),
                     ),
-                    new TextFormField(
-                      maxLines: 1,
-                      keyboardType: TextInputType.number,
-                      autofocus: false,
-                      decoration: new InputDecoration(
-                        hintText: 'Pris',
-                      ),
-                      validator: (value) =>
-                          value.isEmpty ? 'Pris can\'t be empty' : null,
-                      onSaved: (value) => _price = value,
-                    ),
-                    new TextFormField(
-                      maxLines: 1,
-                      keyboardType: TextInputType.text,
-                      autofocus: false,
-                      decoration: new InputDecoration(
-                        hintText: 'Författare',
-                      ),
-                      validator: (value) =>
-                          value.isEmpty ? 'Författare can\'t be empty' : null,
-                      onSaved: (value) => _author = value,
-                    ),
-                    new TextFormField(
-                      maxLines: 1,
-                      keyboardType: TextInputType.text,
-                      autofocus: false,
-                      decoration: new InputDecoration(
-                        hintText: 'ISBN',
-                      ),
-                      validator: (value) =>
-                          value.isEmpty ? 'ISBN can\'t be empty' : null,
-                      onSaved: (value) => _isbn = value,
-                    ),
-                    new TextFormField(
-                      maxLines: 1,
-                      keyboardType: TextInputType.text,
-                      autofocus: false,
-                      decoration: new InputDecoration(
-                        hintText: 'Kontaktinformation',
-                      ),
-                      validator: (value) =>
-                          value.isEmpty ? 'Taggar can\'t be empty' : null,
-                      onSaved: (value) => _contactInfo = value,
+                    FloatingActionButton(
+                      onPressed: getImageCamera,
+                      tooltip: 'Pick Image',
+                      child: Icon(Icons.add_a_photo),
                     ),
                   ],
                 ),
-              ),
+                Container(
+                  padding: EdgeInsets.only(
+                      left: 30.0, top: 10.0, right: 30.0, bottom: 10.0),
+                  child: Form(
+                    key: _advertKey,
+                    child: new Container(
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        color: Color(0xFFFFFFFF),
+                        gradient: new LinearGradient(
+                          colors: [Color(0xFF96070a), Color(0xFFFFFFFF)],
+                          begin: Alignment.centerRight,
+                          end: Alignment.centerLeft,
+                        ),
+                        borderRadius:
+                            BorderRadius.all(Radius.elliptical(20.0, 20.0)),
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          new TextFormField(
+                            maxLines: 1,
+                            keyboardType: TextInputType.text,
+                            autofocus: false,
+                            decoration: new InputDecoration(
+                              hintText: 'Titel',
+                            ),
+                            validator: (value) =>
+                                value.isEmpty ? 'Title can\'t be empty' : null,
+                            onSaved: (value) => _title = value,
+                          ),
+                          new TextFormField(
+                            maxLines: 1,
+                            keyboardType: TextInputType.number,
+                            autofocus: false,
+                            decoration: new InputDecoration(
+                              hintText: 'Pris',
+                            ),
+                            validator: (value) =>
+                                value.isEmpty ? 'Pris can\'t be empty' : null,
+                            onSaved: (value) => _price = value,
+                          ),
+                          new TextFormField(
+                            maxLines: 1,
+                            keyboardType: TextInputType.text,
+                            autofocus: false,
+                            decoration: new InputDecoration(
+                              hintText: 'Författare',
+                            ),
+                            validator: (value) => value.isEmpty
+                                ? 'Författare can\'t be empty'
+                                : null,
+                            onSaved: (value) => _author = value,
+                          ),
+                          new TextFormField(
+                            maxLines: 1,
+                            keyboardType: TextInputType.text,
+                            autofocus: false,
+                            decoration: new InputDecoration(
+                              hintText: 'ISBN',
+                            ),
+                            validator: (value) =>
+                                value.isEmpty ? 'ISBN can\'t be empty' : null,
+                            onSaved: (value) => _isbn = value,
+                          ),
+                          new TextFormField(
+                            maxLines: 1,
+                            keyboardType: TextInputType.text,
+                            autofocus: false,
+                            decoration: new InputDecoration(
+                              hintText: 'Kontaktinformation',
+                            ),
+                            validator: (value) =>
+                                value.isEmpty ? 'Taggar can\'t be empty' : null,
+                            onSaved: (value) => _contactInfo = value,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 150,
+                  child: MaterialButton(
+                    color: Color(0xFF008000),
+                    onPressed: () async {
+                      if (_advertKey.currentState.validate()) {
+                        _advertKey.currentState.save();
+                        _uploadNewAdvert();
+                      }
+                    },
+                    child: Text("Ladda upp",
+                        style: TextStyle(color: Color(0xFFFFFFFF))),
+                  ),
+                ),
+              ],
             ),
           ),
-          Container(
-            width: 150,
-            child: MaterialButton(
-              color: Color(0xFF008000),
-              onPressed: () async {
-                if (_advertKey.currentState.validate()) {
-                  _advertKey.currentState.save();
-                  _uploadNewAdvert();
-                }
-              },
-              child:
-                  Text("Ladda upp", style: TextStyle(color: Color(0xFFFFFFFF))),
-            ),
-          ),
-        ],
-      ),
-    );
+        ),
+      );
   }
 
   @override
@@ -201,7 +216,7 @@ class advertCreationState extends State<advertCreation>
   Future<String> _uploadNewAdvert() async {
     Advert _newAd = new Advert(_title, _price, _author, _isbn, _contactInfo);
     var data = json.encode(_newAd);
-    print (data);
+    print(data);
     String postURL = "http://07487b83.ngrok.io/api/adverts/?format=json";
     return await http.post(Uri.encodeFull(postURL), body: data, headers: {
       "Accept": "application/json",
@@ -221,13 +236,13 @@ class Advert {
   String transaction_type = "S";
   String contactInfo;
 
- Advert(this.title, this.price, this.authors, this.ISBN, this.contactInfo);
+  Advert(this.title, this.price, this.authors, this.ISBN, this.contactInfo);
 
- int intToString(price) {
-   var priceInt = int.parse(price);
-   assert (priceInt is int);
-   return priceInt;
- }
+  int intToString(price) {
+    var priceInt = int.parse(price);
+    assert(priceInt is int);
+    return priceInt;
+  }
 
   Map<String, dynamic> toJson() => {
         'book_title': title,
