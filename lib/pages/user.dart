3e3bedac3bd34998c7e43d683414a8356dee5d44
n3reides <dahlstedtolle@gmail.com>
2019-04-22 16:04:21 +0200

    hi

--- lib/pages/user.dart
+++ lib/pages/user.dart
@@ -14,8 +14,6 @@ class User {
         username = json['username'],
         id = json['id'],
         token = json['token'];
-
-
 }
 
 class UserCreation {
@@ -26,70 +24,73 @@ class UserCreation {
 
   UserCreation(this.email, this.username, this.password, this.imageAsBytes);
 
-  Map<String, dynamic> toJson() => imageAsBytes != null ? {
-    'username': username,
-    'password': password,
-    'email': email,
-    'image': imageAsBytes,
-  }
-  :
-  { 'username': username,
-    'password': password,
-    'email': email,
-  };
+  // om imageAsBytes är null, encode utan den parametern
+  Map<String, dynamic> toJson() => imageAsBytes != null
+      ? {
+          'username': username,
+          'password': password,
+          'email': email,
+          'image': imageAsBytes,
+        }
+      : {
+          'username': username,
+          'password': password,
+          'email': email,
+        };
 }
+
 class UserLogin {
   String username;
   String password;
+
   UserLogin(this.username, this.password);
 
   Map<String, dynamic> toJson() => {
-    'username': username,
-    'password': password,
-  };
+        'username': username,
+        'password': password,
+      };
 }
 
 class UserMethodBody {
   User user;
 
-  void iniUser(String email, int id, String username, String token){
+  void iniUser(String email, int id, String username, String token) {
     user = User(email, id, username, token);
   }
 
-  String getToken(){
+  String getToken() {
     return user.token;
   }
 
-  bool checkUser (){
-    if (user == null){
+  bool checkUser() {
+    if (user == null) {
       return false;
-    }
-    else{
+    } else {
       return true;
     }
   }
 
-  bool loggedIn(){
-    if(user.token == null){
+  bool loggedIn() {
+    if (user.token == null) {
       return false;
-    }
-    else{
+    } else {
       return true;
     }
   }
 
-  Future<bool> register(String email, String username, String password, String imageAsBytes) async {
-    if (imageAsBytes == null) {
-      UserCreation _newUser = new UserCreation(email, username, password, imageAsBytes);
-    }
-    UserCreation _newUser = new UserCreation(email, username, password, imageAsBytes);
+  Future<bool> register(String email, String username, String password,
+      String imageAsBytes) async {
+    UserCreation _newUser =
+        new UserCreation(email, username, password, imageAsBytes);
     var data = json.encode(_newUser);
     print(data);
     String postURL = "https://fecbb9af.ngrok.io/users/create-user/?format=json";
-    var response =  await http.post(Uri.encodeFull(postURL), body: data, headers: {
-      "Accept": "application/json",
-      "content-type": "application/json"
-    });
+    var response = await http.post(Uri.encodeFull(postURL),
+        body: data,
+        headers: {
+          "Accept": "application/json",
+          "content-type": "application/json"
+        });
     final String res = response.body;
     print(json.decode(res));
     Map parsed = json.decode(res);
@@ -98,21 +99,21 @@ class UserMethodBody {
     iniUser(localUser.email, localUser.id, localUser.username, localUser.token);
     if (user != null) {
       return true;
-    }
-    else {
+    } else {
       return false;
     }
   }
-
 
   Future<bool> signIn(String username, String password) async {
     UserLogin _loginUser = new UserLogin(username, password);
     var data = json.encode(_loginUser);
     String postURL = "https://fecbb9af.ngrok.io/users/get-token/?format=json";
-    var response = await http.post(Uri.encodeFull(postURL), body: data, headers: {
-      "Accept": "application/json",
-      "content-type": "application/json"
-    });
+    var response = await http.post(Uri.encodeFull(postURL),
+        body: data,
+        headers: {
+          "Accept": "application/json",
+          "content-type": "application/json"
+        });
     final String res = response.body;
     Map parsed = json.decode(res);
     print(parsed.toString());
@@ -126,9 +127,3 @@ class UserMethodBody {
     }
   }
 }
-
-
-
-
-
-
